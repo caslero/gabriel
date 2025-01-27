ea7b1641df49eaadac42f89791536dba0ee31812
carlos <carlosjperazab@gmail.com>
2025-01-27 12:12:59 -0400

    Probando el modulo de registro, autenticacion por correo y login

--- src/controller/LoginController.js
+++ src/controller/LoginController.js
@@ -9,54 +9,63 @@ export class LoginControlador {
   /** postLogin sen encarga de responder la petision para iniciar sesion en caso
     de cumplirse todas las condiciones */
   static async iniciarSesion(req, res) {
-    const correo = req.body.correo;
-    const clave = req.body.clave;
+    try {
+      const correo = req.body.correo;
+      const clave = req.body.clave;
 
-    let authUsuario = await ModeloUsuarios.usuarioAutorizado(correo);
+      let authUsuario = await ModeloUsuarios.usuarioAutorizado(correo);
 
-    if (!authUsuario[0]) {
-      return res.status(400).json({
-        status: "Error",
-        message: "Usuario no verificado",
-      });
-    }
-
-    if (!authUsuario[0].validar) {
-      const tokenValidarUsuario = await ModeloUsuarios.tokenValidarUsuario(
-        correo
-      );
-      const tokenUnicoValidarEmpleado = tokenValidarUsuario[0].token;
-      const nombre = tokenValidarUsuario[0].nombre;
-
-      EnviarCorreo.sendMailCrearClave(
-        correo,
-        nombre,
-        tokenUnicoValidarEmpleado
-      );
-
-      return res.status(400).json({
-        status: "Error",
-        message: "Valide su correo...",
-      });
-    }
-
-    const resultado = authUsuario[0] && authUsuario[0].clave;
-    const comparada = await bcryptjs.compare(clave, resultado);
-
-    if (comparada) {
-      const token = Tokens.tokenInicioSesion(correo);
-
-      return res
-        .cookie("programacioniii", token.token, token.cookieOption)
-        .json({
-          status: "ok",
-          message: "usuario logueado",
-          redirect: "/comprar-producto",
+      if (!authUsuario[0]) {
+        return res.status(400).json({
+          status: "Error",
+          message: "Usuario no verificado",
         });
-    } else {
-      return res.status(400).json({
-        status: "Error",
-        message: "Credenciales invalidas...",
+      }
+
+      if (!authUsuario[0].validar) {
+        const tokenValidarUsuario = await ModeloUsuarios.tokenValidarUsuario(
+          correo
+        );
+        const tokenUnicoValidarEmpleado = tokenValidarUsuario[0].token;
+        const nombre = tokenValidarUsuario[0].nombre;
+
+        EnviarCorreo.sendMailCrearClave(
+          correo,
+          nombre,
+          tokenUnicoValidarEmpleado
+        );
+
+        return res.status(400).json({
+          status: "Error",
+          message: "Valide su correo...",
+        });
+      }
+
+      const resultado = authUsuario[0] && authUsuario[0].clave;
+      const comparada = await bcryptjs.compare(clave, resultado);
+
+      if (comparada) {
+        const token = Tokens.tokenInicioSesion(correo);
+
+        return res
+          .cookie("programacioniii", token.token, token.cookieOption)
+          .json({
+            status: "ok",
+            message: "usuario logueado",
+            redirect: "/comprar-producto",
+          });
+      } else {
+        return res.status(400).json({
+          status: "Error",
+          message: "Credenciales invalidas...",
+        });
+      }
+    } catch (error) {
+      console.log("Error, al intentar iniciar sesion: " + error);
+      return res.status(500).json({
+        status: "error",
+        numero: 0,
+        message: "Error, no se pudo iniciar sesion...",
       });
     }
   }
