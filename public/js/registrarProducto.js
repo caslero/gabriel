559c5b1347fab49535ffdadd49576d4c9e22e731
carlos <carlosjperazab@gmail.com>
2025-01-30 13:03:30 -0400

    Agregamos el formulario de registro de productos

--- public/js/registrarProducto.js
+++ public/js/registrarProducto.js
@@ -1,1 +1,16 @@
 import { consultarUsuarioActivo } from "./comprar.js";
+
+const agregarNuevoProducto = document.getElementById(
+  "form-registrar-nuevo-producto"
+);
+
+agregarNuevoProducto.addEventListener("submit", guardarProducto);
+
+async function guardarProducto(e) {
+  try {
+    const nombre = e.target.value;
+    console.log(nombre);
+  } catch (error) {
+    console.log("Error, al guardar unnuevo producto");
+  }
+}
