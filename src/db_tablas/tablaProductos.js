cd4f0ae7d064547d538bef9c780ca28b74829197
Gabriel Bolivar <165972978+Gabriel23BL@users.noreply.github.com>
2025-02-08 23:21:52 -0400

    se realizo el registro de los 9 product en la db
    se volvio a realizar el registro de la manera asignada por el profesor

--- src/db_tablas/tablaProductos.js
+++ src/db_tablas/tablaProductos.js
@@ -1,36 +1,30 @@
 export async function crearTablaProductosSqlite(conectar) {
-    try {
+  try {
       const resultado = await conectar.get(`
-        SELECT name 
-        FROM sqlite_master 
-        WHERE type='table' AND name='productos';
+          SELECT name 
+          FROM sqlite_master 
+          WHERE type='table' AND name='productos';
       `);
-  
+
       if (!resultado) {
-        await conectar.run(`
-          CREATE TABLE productos (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nombre VARCHAR(100) NOT NULL,
-            imagen VARCHAR(100) NOT NULL,
-            version INTEGER NOT NULL,
-            ram INTEGER NOT NULL,
-            rom INTEGER NOT NULL,
-            precio REAL NOT NULL,
-            size REAL NOT NULL, 
-            resolucion VARCHAR(50) NOT NULL,
-            procesador VARCHAR(100),
-            bateria INTEGER NOT NULL,
-            camara VARCHAR(20),
-            id_categoria INTEGER NOT NULL,
-            fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (id_categoria) REFERENCES categorias(id)
-          )
-        `);
-        console.log("Tabla de productos creada.");
+          await conectar.run(`
+              CREATE TABLE productos (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  codigo VARCHAR(10) NOT NULL UNIQUE,
+                  producto VARCHAR(100) NOT NULL,
+                  imagen VARCHAR(150) NOT NULL,
+                  id_categoria INTEGER NOT NULL,
+                  existencia_actual INTEGER NOT NULL DEFAULT 0,  -- Cambiado a INTEGER para representar true/false
+                  precio REAL NOT NULL,
+                  fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                  FOREIGN KEY (id_categoria) REFERENCES categorias(id)
+              )
+          `);
+          console.log("Tabla de productos creada.");
       } else {
-        console.log("La tabla de productos ya existe.");
+          console.log("La tabla de productos ya existe.");
       }
-    } catch (error) {
+  } catch (error) {
       console.error("Error al crear la tabla de productos:", error);
-    }
   }
+}
