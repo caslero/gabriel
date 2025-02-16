9c9fa0e939b881e10119e8d6d980bf49d4c899f3
carlos <carlosjperazab@gmail.com>
2025-02-16 14:40:13 -0400

    Prueba

---
+++ public/js/cambiarClave.js
@@ -0,0 +1,69 @@
+import { direccionLocal } from "./constantes.js";
+
+
+/** entrarAlSistema recibe los datos del formulario para hacer login */
+const cambiarClave = document.getElementById('changePasswordForm');
+//const msjUsuario = document.getElementById("registroExitosoUsuario");
+/** msj muestra los msj de advertencia al intentar hacer login */
+const msj = document.getElementById("validar");
+/** divMsj imprime los msj */
+const divMsj = document.getElementById("validar");
+
+cambiarClave.addEventListener('submit', cambiandoClave);
+
+/** cambiandoClave envia los datos correspondientes para iniciar sesion */
+async function cambiandoClave(e) {
+  e.preventDefault();
+  const claveVieja = e.target.correo.value;
+  const claveNueva = e.target.clave.value;
+  const claveNuevaConfirmar = e.target.clave.value;
+  
+  const respuesta = await fetch(`${direccionLocal}/api/cambiar-clave-loggueado`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      claveVieja: claveVieja,
+      claveNuevaUno: claveNueva,
+      clavDos: claveNuevaConfirmar,
+    })
+  })
+
+  const respuestaJson = await respuesta.json();
+  const statusError = respuestaJson.status;
+  const mensaje = respuestaJson.message;  
+
+  console.log(respuestaJson);
+  
+ 
+  if (statusError == 'error') {    
+    mensajesValidacion(mensaje)
+  } else {
+    window.location.href = respuestaJson.redirect;
+  }
+
+}
+
+/** mensajesValidacion recibe los mensajes al iniciar sesion y muestra la respuesta */
+async function mensajesValidacion(mensaje) {
+  
+    toggleCuadroMensajes()
+    divMsj.innerHTML = `<div class="text-[20px]">${mensaje}</div>`;
+    toggleMensajes();
+}
+
+/** toggleMensajes se encarga de ocultar el msj de la respuesta */
+async function toggleMensajes() {
+  setTimeout(() => {
+    msj.classList.remove("mostrarMsjValidacion");
+    msj.classList.add("ocultarMsjValidacion");
+    divMsj.innerHTML = '';
+  }, "3000");
+}
+
+/** toggleCuadroMensajes se encarga de ocultar el cuadro de los mensajes */
+async function toggleCuadroMensajes() {
+  msj.classList.remove("ocultarMsjValidacion");
+  msj.classList.add("mostrarMsjValidacion");
+}
